f2fa448ca1fe208a8edef06c9cf344a11d7bed2e
daemonprocellae@live.it <daemonprocellae@live.it>
2022-02-08 17:42:56 +0100

    Aggiunti commenti

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
+// assegno alla variabile l'icona hamburger
 const hambMenu = document.querySelector(".fa-bars");
-console.log(hambMenu);
+console.log(hambMenu); //verifico sia stata presa correttamente
 
+//event listener click sull'icona hamburger
 hambMenu.addEventListener("click", function() 
 {
     // alert(`it's working`);
-    document.querySelector(".hamburger-menu").style.display = "block";
+    document.querySelector(".hamburger-menu").style.display = "block"; //apro il menu
 });
 
+//event listener click sull'icona x
 const close = document.querySelector(".close");
 close.addEventListener("click", function() 
 {
-    console.log("hai premuto sulla x");
-    document.querySelector(".hamburger-menu").style.display = "none";
+    // console.log("hai premuto sulla x");
+    document.querySelector(".hamburger-menu").style.display = "none"; //chiudo il menu
 });
